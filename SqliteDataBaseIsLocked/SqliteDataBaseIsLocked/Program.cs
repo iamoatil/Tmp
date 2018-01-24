27a13b0c5d8128a9280c54de2f8032af3f185830
iamoatil <iamoatil@163.com>
2018-01-24 18:17:39 +0800

    加入SqliteDataBaseIsLocked临时代码

---
+++ SqliteDataBaseIsLocked/SqliteDataBaseIsLocked/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SqliteDataBaseIsLocked
+{
+    class Program
+    {
+        static void Main()
+        {
+            string file = "data.db";
+            file = Path.GetFullPath(file);
+            
+            Program p = new Program();
+            p.Initialize(file);
+            for (int i = 0; i < 100; i++)
+            {
+                Task.Run(() =>
+                {
+                    p.Write(file);
+                });
+            }
+            
+            Console.Read();
+        }
+
+        private Object _readWriteLocker = new object();
+
+        public bool IsInitialized { get; private set; }
+
+        public void Initialize(string dbFile)
+        {
+            IsInitialized = false;
+            SqliteRWConnection.CreateFile(dbFile);
+
+            SQLiteConnectionStringBuilder sb = new SQLiteConnectionStringBuilder();
+            sb.DataSource = dbFile;
+            using (SqliteRWConnection connection = new SqliteRWConnection(sb))
+            {
+                connection.Open();               
+                using (SQLiteCommand cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = "Create Table MyTable(name varchar(20), score int)";
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = "insert into MyTable (name, score) values ('Me', 3000)";
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = "insert into MyTable (name, score) values ('You', 4000)";
+                    cmd.ExecuteNonQuery();
+                }
+            }    
+            
+            IsInitialized = true;
+        }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void Read(string dbFile)
+        {
+            SQLiteConnectionStringBuilder sb = new SQLiteConnectionStringBuilder();
+            sb.DataSource = dbFile;
+            SqliteRWConnection connection = new SqliteRWConnection(sb);
+            connection.Open();
+            using (SQLiteCommand cmd = connection.CreateCommand())
+            {
+                try
+                {
+                    while (true)
+                    {
+                        //lock (_readWriteLocker)
+                        {
+                            cmd.CommandText = "Select * from MyTable";
+                            using (SQLiteDataReader reader = cmd.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    string name = reader["name"].ToString();
+                                    string score = reader["score"].ToString();
+                                    Console.WriteLine($"name:{name}  Score:{score}");
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+                
+            }
+        }
+
+        SqliteRWConnection connection;
+        public void Write(string dbFile)
+        {
+            SQLiteConnectionStringBuilder sb = new SQLiteConnectionStringBuilder();
+            sb.DataSource = dbFile;
+            //if (connection == null)
+            {
+                connection = new SqliteRWConnection(sb);
+                connection.Open();
+            }
+            
+            using (SQLiteCommand cmd = connection.CreateCommand())
+            {
+                try
+                {
+                    while (true)
+                    {
+                       // lock (_readWriteLocker)
+                        {
+                            cmd.CommandText = "Update MyTable set score = 0 where score = 4000;" +
+                            "Update MyTable set score = 4000 where score = 3000;" +
+                            "Update MyTable set score = 3000 where score = 0;";
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+                
+            }            
+        }
+    }
+
+    public class WriteProgram
+    {
+        public bool IsInitialized { get; private set; }
+
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        public void Initialize()
+        {
+            IsInitialized = false;
+
+            IsInitialized = true;
+        }
+    }
+}
---
+++ SqliteDataBaseIsLocked/SqliteDataBaseIsLocked/SqliteSingleRW.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace SqliteDataBaseIsLocked
+{
+    public class SqliteRWConnection:IDisposable
+    {
+        private readonly static Dictionary<string, SQLiteConnection> _dbFileDic = new Dictionary<string, SQLiteConnection>();
+
+        /// <summary>
+        /// 连接对应的文件
+        /// </summary>
+        private string _curDbfile;
+
+        /// <summary>
+        /// 真实的连接
+        /// </summary>
+        private SQLiteConnection _curConnection;
+        
+        /// <summary>
+        /// 被引用的个数
+        /// </summary>
+        private int _refCount = 0;
+
+        /// <summary>
+        /// 对_dbFileDic进行维护的锁
+        /// </summary>
+        private object _simpleLocker = new object();
+
+        public SqliteRWConnection(SQLiteConnectionStringBuilder connectObject)
+        {
+            lock (_simpleLocker)
+            {
+                _curDbfile = connectObject.DataSource;
+                if (!_dbFileDic.ContainsKey(_curDbfile))
+                {
+                    SQLiteConnection connection = new SQLiteConnection(connectObject.ConnectionString);
+                    connection.Open();
+                    _dbFileDic.Add(_curDbfile, connection);
+                    _refCount = 0;
+                }
+                _refCount++;
+                _curConnection = _dbFileDic[_curDbfile];
+            }
+        }
+
+        internal static void CreateFile(string dbFile)
+        {
+            SQLiteConnection.CreateFile(dbFile);
+        }
+
+        internal void Open()
+        {
+            if(_curConnection.State == System.Data.ConnectionState.Closed)
+            {
+                _curConnection.Open();
+            }
+        }
+
+        internal SQLiteCommand CreateCommand()
+        {
+            return _curConnection.CreateCommand();
+        }
+
+        #region IDisposable
+        //是否回收完毕
+        bool _disposed;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        ~SqliteRWConnection()
+        {
+            Dispose(false);
+        }
+
+        //这里的参数表示示是否需要释放那些实现IDisposable接口的托管对象
+        protected virtual void Dispose(bool disposing)
+        {
+            lock (_simpleLocker)
+            {
+                _refCount--;
+                if(_refCount > 0)
+                {
+                    return;
+                }
+                _dbFileDic.Remove(_curDbfile);
+                
+                if (_disposed)
+                {
+                    return; //如果已经被回收，就中断执行
+                }
+                if (disposing)
+                {
+                    _curConnection.Dispose();
+                    //TODO:释放那些实现IDisposable接口的托管对象
+                }
+                //TODO:释放非托管资源，设置对象为null
+                _disposed = true;
+            }
+        }
+        #endregion        
+    }
+}
